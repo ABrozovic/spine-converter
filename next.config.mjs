0cca8b042cbb1725ba550182c1426fcf7ead9145
Z <alejandro.brozovic@gmail.com>
2023-07-26 15:19:16 -0400

    chore: disable nextjs minimization

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,14 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
-  // compress: false,
-  // webpack(webpackConfig) {
-  //   return {
-  //     ...webpackConfig,
-  //     optimization: {
-  //       minimize: false,
-  //     },
-  //   };
-  // },
+  webpack(webpackConfig) {
+    return {
+      ...webpackConfig,
+      optimization: {
+        minimize: false,
+      },
+    };
+  },
 };
 
 export default config;
