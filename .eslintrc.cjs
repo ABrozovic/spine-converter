daab125edc1211d5bc525ab82fa73076310c62e5
Z <alejandro.brozovic@gmail.com>
2023-07-25 20:55:52 -0400

    chore: update eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,30 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "tailwindcss"],
   extends: [
     "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:tailwindcss/recommended",
+    "prettier",
   ],
-  rules: {    
+  settings: {
+    tailwindcss: {
+      callees: ["cn"],
+      config: "./tailwind.config.js",
+    },
+    next: {
+      rootDir: ["./"],
+    },
+  },
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -20,6 +34,12 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
-};
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+}
 
-module.exports = config;
+module.exports = config
